2dbaa70108819a8abd1ccd6618ee0901c93b5552
madhavsikka <madhavsikka99@gmail.com>
2021-09-22 09:06:14 +0530

    Initial Commit

---
+++ PDFTemp/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  PDFTemp
+//
+//  Created by Madhav Sikka on 22/09/21.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
